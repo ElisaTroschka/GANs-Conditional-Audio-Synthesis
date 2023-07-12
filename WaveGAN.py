7c45112d639d69e3c400f0beabcbce8ed432c02e
Elisa Troschka <adcy353@gpu04.pri.hyperion.alces.network>
2023-07-12 10:12:36 +0100

    training

--- WaveGAN.py
+++ WaveGAN.py
@@ -3,37 +3,41 @@ import torch.nn as nn
 
 
 class WaveGANGenerator(nn.Module):
-    def __init__(self, in_dim, cond_dim, out_dim, sr, duration, kernel_len=25):
+    def __init__(self, in_dim, cond_dim, out_dim, sr, duration, in_ch=1, kernel_len=25):
         super(WaveGANGenerator, self).__init__()
         self.in_dim = in_dim
         self.cond_dim = cond_dim
         self.out_dim = out_dim
         self.sr = sr
         self.duration = duration
+        self.kernel_len = kernel_len
         self.slice_len = int(self.sr * self.duration)
 
         if duration <= 0:
             raise ValueError("Duration must be greater than 0.")
-
-        self.generator = nn.Sequential(
-            nn.Linear(self.in_dim + self.cond_dim, 100),
-            self._make_conv_block(100, 64, kernel_size=kernel_len),
+        self.fc = nn.Linear(self.in_dim + self.cond_dim, 100)
+        self.deconv = nn.Sequential(
+            self._make_conv_block(in_ch, 64, kernel_size=kernel_len),
             self._make_conv_block(64, 256, kernel_size=kernel_len),
             self._make_conv_block(256, 1024, kernel_size=kernel_len),
             self._make_conv_block(1024, 4096, kernel_size=kernel_len),
-            self._make_conv_block(4096, self.slice_len, kernel_size=kernel_len),
+            nn.ConvTranspose1d(4096, self.slice_len, kernel_len, stride=4, padding=160),
             nn.Tanh()
         )
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+
 
     def forward(self, x, cond):
-        x = torch.cat([x, cond], dim=1)
-        output = self.generator(x)
+        output = torch.cat([x, cond], dim=1)
+        output = self.fc(output).unsqueeze(1)
+        output = self.deconv(output)
+        output = self.avgpool(output).squeeze()
         output = self.adjust_output_length(output)
         return output
 
     def _make_conv_block(self, input_dim, output_dim, kernel_size):
         return nn.Sequential(
-            nn.ConvTranspose1d(input_dim, output_dim, kernel_size, stride=4, padding=0, bias=True, padding_mode='same'),
+            nn.ConvTranspose1d(input_dim, output_dim, kernel_size, stride=4, padding=159),#//2, bias=True),
             nn.ReLU()
         )
 
@@ -44,33 +48,32 @@ class WaveGANGenerator(nn.Module):
 
 
 class WaveGANDiscriminator(nn.Module):
-    def __init__(self, in_dim, cond_dim, kernel_len=25, phaseshuffle_rad=0):
+    def __init__(self, in_dim, cond_dim, kernel_len=25, in_ch=1, phaseshuffle_rad=0):
         super(WaveGANDiscriminator, self).__init__()
 
         self.lrelu = nn.LeakyReLU(0.2)
         self.phaseshuffle_rad = phaseshuffle_rad
 
         self.conv_layers = nn.Sequential(
-            nn.Conv1d(in_dim + cond_dim, 64, kernel_len, stride=4, padding=kernel_len//2),
-            nn.Conv1d(64, 128, kernel_len, stride=4, padding=kernel_len//2),
+            nn.Conv1d(in_ch, 64, kernel_len, stride=4, padding=kernel_len//2), # (50, 1, in__dim + cond_dim)
+            nn.Conv1d(64, 128, kernel_len, stride=4, padding=kernel_len//2), # (50, 64, ) ((L−1)/4)+1
             nn.Conv1d(128, 256, kernel_len, stride=4, padding=kernel_len//2),
             nn.Conv1d(256, 512, kernel_len, stride=4, padding=kernel_len//2),
             nn.Conv1d(512, 1024, kernel_len, stride=4, padding=kernel_len//2),
             nn.Conv1d(1024, 2048, kernel_len, stride=2 if phaseshuffle_rad > 0 else 4, padding=kernel_len//2)
         )
-
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
         self.linear = nn.Linear(2048, 1)
 
     def forward(self, x, cond):
-        output = torch.cat([x, cond], dim=1)
+        output = torch.cat([x, cond], dim=1).unsqueeze(1)        
         for layer in self.conv_layers:
             output = layer(output)
             output = self.lrelu(output)
-            output = self._apply_phase_shuffle(output)
-
-        output = output.view(output.size(0), -1)
-        output = self.linear(output)
-        output = output.squeeze()
+            output = self._apply_phase_shuffle(output, self.phaseshuffle_rad)
+        output = self.avgpool(output).squeeze()
+        output = self.linear(output).squeeze()
+        #output = nn.Sigmoid(output)
 
         return output
 
--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from WaveGAN import WaveGANGenerator, WaveGANDiscriminator
 
 
-def train(train_set, batch_size, lr,  epochs, z_size, verbose=50):
+def train(train_set, batch_size, lr,  epochs, z_size, dg_ratio=5, verbose=50):
 
     G_losses = []
     D_losses = []
@@ -16,15 +16,19 @@ def train(train_set, batch_size, lr,  epochs, z_size, verbose=50):
     print(f"Working on {device}")
 
     # Loading the data
-    trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
+    print('Loading data...')
+    trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
     # Creating a fixed noise vectors
     fixed_z = torch.rand(batch_size, z_size, device=device)
 
 
     # Creating the generator and discriminator
-    G = WaveGANGenerator(z_size, train_set.label_size, train_set.y_size, train_set.sample_rate, train_set.duration).to(device)
+    print('Creating WaveGAN...')
+    G = WaveGANGenerator(z_size, train_set.label_size, train_set.y_size, train_set.sampling_rate, train_set.duration).to(device)
+    print(G)
     D = WaveGANDiscriminator(train_set.y_size, train_set.label_size).to(device)
+    print(D)
 
     # Creating optimizers
     G_optim = optim.Adam(G.parameters(), lr, betas=(0.5, 0.9))
@@ -35,36 +39,48 @@ def train(train_set, batch_size, lr,  epochs, z_size, verbose=50):
 
     # Train loop
     for epoch in range(epochs):
+        if epoch % verbose == 0:
+            t = torch.cuda.get_device_properties(0).total_memory
+            r = torch.cuda.memory_reserved(0)
+            a = torch.cuda.memory_allocated(0)
+            f = r-a  # free inside reserved
+            print(f'\033[94mtotal_memory: {t}, available_memory: {f}\033[0m')
+            
         loss_G_batch = []
         loss_D_batch = []
         for i, data in enumerate(tqdm(trainloader, leave=True)):
 
             x, cond = data
+            x, cond = x.to(device), cond.to(device)
             ####################################################################
             # Training the discriminator: maximising log(D(x)) + log(1 - D(G(x))
             ####################################################################
-            D.zero_grad()
-            # Training with real batch
-            y = torch.full((batch_size,), 1, dtype=torch.float, device=device)
-            real = D(x, cond)
-            loss_D_real = criterion(real, y)
-            loss_D_real.backward()
+            for _ in range(dg_ratio):
+                D.zero_grad()
+                # Training with real batch
+                y_r = torch.full((batch_size,), 1, dtype=torch.float, device=device)
+                real = D(x, cond)
+                loss_D_real = criterion(real, y_r)
+                loss_D_real.backward()
 
-            # Training with fake batch
-            z = torch.rand(batch_size, z_size, device=device)
-            y = torch.full((batch_size,), 0, dtype=torch.float, device=device)
-            fake = G(z, cond)
-            loss_D_fake = criterion(fake.detach(), y)
-            loss_D_fake.backward()
-            loss_D = loss_D_real + loss_D_fake
-            D_optim.step()
+                # Training with fake batch
+                #D.zero_grad()
+                z = torch.rand(batch_size, z_size, device=device)
+                y_f = torch.full((batch_size,), 0, dtype=torch.float, device=device)
+                G_out = G(z, cond)
+                fake = D(G_out, cond)
+                loss_D_fake = criterion(fake.detach(), y_f)
+                loss_D_fake.requires_grad = True
+                loss_D_fake.backward()
+                loss_D = loss_D_real + loss_D_fake
+                D_optim.step()
 
             ####################################################################
             # Training the generator: maximising log(D(G(x))
             ####################################################################
             G.zero_grad()
             y = torch.full((batch_size,), 1, dtype=torch.float, device=device)
-            output = D(fake, cond)
+            output = D(G_out, cond)
             loss_G = criterion(output, y)
             loss_G.backward()
             G_optim.step()
@@ -73,12 +89,14 @@ def train(train_set, batch_size, lr,  epochs, z_size, verbose=50):
             loss_D_batch.append(loss_D)
 
         # Output
-        loss_G_epoch = loss_G_batch.mean()
-        loss_D_epoch = loss_D_batch.mean()
+        loss_G_epoch = torch.mean(torch.tensor(loss_G_batch))
+        loss_D_epoch = torch.mean(torch.tensor(loss_D_batch))
 
         if epoch % verbose == 0:
-            print(f'\n\033[94m;1mEPOCH {epoch}\033[0m Generator loss: {loss_G_epoch.item()}, Discriminator loss: {loss_D_epoch.item()}')
+            print(f'\n\033[1mEPOCH {epoch + 1}/{epochs}:\033[0m Generator loss: {loss_G_epoch.item()}, Discriminator loss: {loss_D_epoch.item()}')
 
         G_losses.append(loss_G_epoch)
         D_losses.append(loss_D_epoch)
+    
+    return G_losses, D_losses, G, D
 
